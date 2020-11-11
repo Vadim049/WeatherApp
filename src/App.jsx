a39979b514063b68ad5893aad03c9838cbd43a74
romos21 <romos2606@gmal.com>
2020-11-11 19:52:01 +0300

    fixed defects (warn: url404)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-
 import './App.css';
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import WeatherDisplay from "./WeatherDisplay.jsx";
 
 import { Navbar, NavItem, Nav, Container, Row, Col } from 'react-bootstrap';
-
-
 
 const PLACES = [
     { name: "Minsk", zip: "220000" },
@@ -14,44 +12,7 @@ const PLACES = [
     { name: "Vitebsk", zip: "210000" }
 ];
 
-
-class WeatherDisplay extends Component {
-    constructor() {
-        super();
-        this.state = {
-            weatherData: null
-        };
-    }
-    componentDidMount() {
-        const zip = this.props.zip;
-        const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
-            zip +
-            "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
-        fetch(URL).then(res => res.json()).then(json => {
-            this.setState({ weatherData: json });
-        });
-    }
-    render() {
-        const weatherData = this.state.weatherData;
-        if (!weatherData) return <div>Loading</div>;
-        const weather = weatherData.weather[0];
-        const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
-        return (
-            <div>
-                <h1>
-                    {weather.main} in {weatherData.name}
-                    <img src={iconUrl} alt={weatherData.description} />
-                </h1>
-                <p>Current: {weatherData.main.temp}°</p>
-                <p>High: {weatherData.main.temp_max}°</p>
-                <p>Low: {weatherData.main.temp_min}°</p>
-                <p>Wind Speed: {weatherData.wind.speed} mi/hr</p>
-            </div>
-        );
-    }
-}
-
-class App extends Component {
+export default class App extends Component {
     constructor() {
         super();
         this.state = {
@@ -63,26 +24,25 @@ class App extends Component {
         return (
             <div>
                 <Navbar>
-                    <Navbar.Header>
+                    <Navbar.Text>
                         <Navbar.Brand>
                             React Simple Weather App
                         </Navbar.Brand>
-                    </Navbar.Header>
+                    </Navbar.Text>
                 </Navbar>
                 <Container>
                     <Row>
                         <Col md={4} sm={4}>
                             <h3>Select a city</h3>
                             <Nav
-                                bsStyle="pills"
-                                stacked
+                                bsstyle="pills"
                                 activeKey={activePlace}
                                 onSelect={index => {
                                     this.setState({ activePlace: index });
                                 }}
                             >
                                 {PLACES.map((place, index) => (
-                                    <NavItem key={index} eventKey={index}>{place.name}</NavItem>
+                                    <NavItem key={index} eventkey={index}>{place.name}</NavItem>
                                 ))}
                             </Nav>
                         </Col>
@@ -95,5 +55,3 @@ class App extends Component {
         );
     }
 }
-
-export default App;
